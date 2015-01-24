08764e1505fce9a1677e66c332555ece47826934
Jon Chretien <jchretien@spotify.com>
2015-01-24 16:59:35 -0500

    serve static files from root directory

--- server.js
+++ server.js
@@ -3,17 +3,20 @@
 'use strict';
 
 /**
- * Module dependencies
+ * Module dependencies.
  */
 var express = require('express');
 
+/**
+ * Initialize variables.
+ */
 var app = express();
 var PORT = 8000;
 
 /**
  * Serve static files.
  */
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname));
 app.listen(process.env.PORT || PORT);
 
 console.log('Server running at http://127.0.0.1:' + PORT + '/');
