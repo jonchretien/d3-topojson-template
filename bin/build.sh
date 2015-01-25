5662d18246264ab67426abfac541df9036c73274
Jon Chretien <jchretien@spotify.com>
2015-01-25 10:58:45 -0500

    echo as build commands run

--- bin/build.sh
+++ bin/build.sh
@@ -5,16 +5,19 @@ BUILD_DIR='_dist'
 
 # remove previous build directory
 rm -rf $BUILD_DIR/
+echo "Removed old build directory."
 
 # make directories
 mkdir $BUILD_DIR
 mkdir $BUILD_DIR/css
 mkdir $BUILD_DIR/js
 mkdir $BUILD_DIR/data
+echo "Created new directories."
 
 # copy files
 cp index.html $BUILD_DIR/index.html
-cp css/bundle.css $BUILD_DIR/css/app.css
+cp css/bundle.css $BUILD_DIR/css/bundle.css
 cp data/us-states-output.json $BUILD_DIR/data/us-states-output.json
 cp data/data.json $BUILD_DIR/data/data.json
 cp js/bundle.js $BUILD_DIR/js/bundle.js
+echo "Copied files."
